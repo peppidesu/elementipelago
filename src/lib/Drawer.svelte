ec1d2470c3ea8baee354edfb636975af416a504d
Noa Aarts <noa@voorwaarts.nl>
2026-01-06 19:20:09 +0100

    :3

--- src/lib/Drawer.svelte
+++ src/lib/Drawer.svelte
@@ -1,9 +1,24 @@
 <script>
     import Element from "./Element.svelte";
+
+    const modules = import.meta.glob("../assets/Elements/*.png", {
+        eager: true,
+        import: "default",
+    });
+
+    const el = Object.fromEntries(
+        Object.entries(modules).map(([path, url]) => {
+            const name = path.split("/").pop().replace(".png", "");
+            return [name, url];
+        }),
+    );
+
+    console.log(el);
+
     let elements = [
-        { name: "waaa", src: "" },
-        { name: "baba", src: "" },
-        { name: "Noa", src: "" },
+        { name: "waaa", src: el.apple },
+        { name: "baba", src: el.book },
+        { name: "Noa", src: el.coin },
     ];
 </script>
 
