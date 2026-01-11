0bbb48b13904e7ea7f91ca5f1e440a66ac35a549
Noa Aarts <noa@voorwaarts.nl>
2026-01-11 17:46:43 +0100

    feat: add icons to the elements that get spawned as well

--- src/consts.js
+++ src/consts.js
@@ -1,1 +1,13 @@
 export const LOCATION_AMOUNT = 2000;
+
+const modules = import.meta.glob("../assets/Elements/*.png", {
+    eager: true,
+    import: "default",
+});
+
+export const element_urls = Object.fromEntries(
+    Object.entries(modules).map(([path, url]) => {
+        const name = path.split("/").pop().replace(".png", "");
+        return [name, url];
+    }),
+);
