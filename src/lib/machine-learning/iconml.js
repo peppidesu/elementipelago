572d1a223c6c5f5e7089cc7dc6222bc13e7da580
Noa Aarts <noa@voorwaarts.nl>
2026-01-26 23:47:38 +0100

    generate substitute element list

--- src/lib/machine-learning/iconml.js
+++ src/lib/machine-learning/iconml.js
@@ -22,65 +22,90 @@ export function iconForItem(game, name) {
     return iconForText(`[game=${game}] ${name}`);
 }
 
+const colors = [
+    "lightblue",
+    "turqoise",
+    "green",
+    "lime",
+    "yellow",
+    "orange",
+    "warmred",
+    "red",
+    "magenta",
+    "purple",
+    "indigo",
+    "blue",
+    "white"
+];
+
+const substituteIcons = [
+    "apple",
+    "armor",
+    "ball",
+    "berry",
+    "boat",
+    "book",
+    "element",
+    "emerald",
+    "car",
+];
+
+const icons = [
+    "boots",
+    "bow",
+    "cave",
+    "chest",
+    "coin",
+    "desert",
+    "egg",
+    "fire",
+    "gun",
+    "hammer",
+    "hat",
+    "heart",
+    "hills",
+    "hourglass",
+    "house",
+    "ice",
+    "island",
+    "key",
+    "leaf",
+    "magic",
+    "map",
+    "marker",
+    "metal",
+    "money",
+    "mountains",
+    "music",
+    "piece",
+    "planet",
+    "potion",
+    "quest",
+    "ring",
+    "rock",
+    "sand",
+    "shop",
+    "sign",
+    "skull",
+    "spear",
+    "sword",
+    "tree",
+    "upgrade",
+    "void",
+    "wand",
+    "water"
+];
+
+const combinedIcons = [...icons, ...substituteIcons.flatMap((value) => colors.map((color) => `${value}-${color}`))]
+
+/**
+ * @param {String} text
+ * @returns {String}
+ */
 function iconForText(text) {
     //const res = predictIcon(get(model), text, { returnTopK: 1 });
-
-    const icons = [
-        "apple",
-        "armor",
-        "ball",
-        "berry",
-        "boat",
-        "book",
-        "boots",
-        "bow",
-        "car",
-        "cave",
-        "chest",
-        "coin",
-        "desert",
-        "egg",
-        "element",
-        "emerald",
-        "fire",
-        "gun",
-        "hammer",
-        "hat",
-        "heart",
-        "hills",
-        "hourglass",
-        "house",
-        "ice",
-        "island",
-        "key",
-        "leaf",
-        "magic",
-        "map",
-        "marker",
-        "metal",
-        "money",
-        "mountains",
-        "music",
-        "piece",
-        "planet",
-        "potion",
-        "quest",
-        "ring",
-        "rock",
-        "sand",
-        "shop",
-        "sign",
-        "skull",
-        "spear",
-        "sword",
-        "tree",
-        "upgrade",
-        "void",
-        "wand",
-        "water"
-    ];
     const buffer = new Uint32Array(md5.arrayBuffer(text));
-    const res = icons[buffer[buffer.length - 1] % icons.length];
+    const res = combinedIcons[buffer[buffer.length - 1] % combinedIcons.length];
 
     const iconKey = res;
     return iconKey;
